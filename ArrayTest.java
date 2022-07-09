519336d88656e8e058ee9d014fbc329a597363b8
SYAM49-sudo <105502093+SYAM49-sudo@users.noreply.github.com>
2022-07-09 17:37:25 +0000

    Add files via upload
    Array

---
+++ ArrayTest.java
@@ -0,0 +1,16 @@
+package Nothing;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		//Create an array
+		int[] a = {1,7,8,11};  //length --> Pre_Defined
+		System.out.println("Array_Length : "+a.length);
+		for(int i=0;i<a.length;i++) {
+			System.out.println("Array-"+i+" : "+a[i]);
+		}
+		
+
+	}
+
+}
